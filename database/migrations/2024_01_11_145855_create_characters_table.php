a1a531550fcccf84f21eff2f31445f2dbbbdbef0
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-01-11 17:26:12 +0100

    migration character

--- database/migrations/2024_01_11_145855_create_characters_table.php
+++ database/migrations/2024_01_11_145855_create_characters_table.php
@@ -4,17 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('characters', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        Schema::create(
+            'characters',
+            function (Blueprint $table) {
+                $table->id();
+                $table->string('name', 200);
+                $table->text('description')->nullable();
+                $table->int('type_id');
+                $table->int('attack');
+                $table->int('defence');
+                $table->int('speed');
+                $table->int('life');
+                $table->timestamps();
+            }
+        );
     }
 
     /**
@@ -23,5 +32,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('characters');
+
     }
 };
