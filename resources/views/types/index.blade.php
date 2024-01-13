a6447ea48e46bee34c6e3dc603c003eec4e0a3de
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-01-13 16:06:24 +0100

    Co-authored-by: fcarrara24 <fcarrara24@users.noreply.github.com>

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 @section('content')
     <section class="container mt-5">
-        <h2>INDEX TYPE</h2>
+        <a href="{{ route('type.create') }}" style="margin-bottom: 40px; color:red;">
+            New type
+        </a>
+        @foreach ($types as $type)
+            <div>
+                {{ $type['name'] }}
+            </div>
+            <a href="{{ route('type.show', $type->id) }}">
+                info
+            </a>
+            <form action="{{ route('type.destroy', $type->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger cancel-button" type="submit">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </form>
+        @endforeach
+        @include('partials.modal_delete')
     </section>
 @endsection
--- resources/views/types/show.blade.php
+++ resources/views/types/show.blade.php
@@ -1,1 +1,30 @@
+@extends('layouts.app')
 
+@section('content')
+    <h2>Show Type</h2>
+
+
+
+    <div>{{ $type['name'] }}</div>
+    <div>{{ $type['description'] }}</div>
+
+    <form action="{{ route('type.destroy', $type->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button class="btn btn-danger cancel-button" type="submit">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+    </form>
+
+    <form action="{{ route('type.edit', $type->id) }}" method="GET">
+        @csrf
+        <button class="btn btn-warning" type="submit">
+            <i class="fa-solid fa-wrench"></i>
+        </button>
+    </form>
+
+    <a href="{{ route('type.index') }}" style="margin-bottom: 40px; color:red;">
+        back
+    </a>
+    @include('partials.modal_delete')
+@endsection
