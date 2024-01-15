afbbb25aef7e6c59357643408958468828ab7d9a
Trotteriniku <stefanozidda3@gmail.com>
2024-01-15 16:45:07 +0100

    update login and register layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <section class="container mt-5">
+    <section class="container mt-5 text-light">
         <h1>Section title</h1>
         <p>section content</p>
     </section>
