01cafceb9eef6c7d51566976c1f2edf405ab2427
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-01-24 16:40:38 +0100

    updated paginate items

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -10,7 +10,7 @@ class ItemController extends Controller
 {
     public function index()
     {
-        $items = Item::all();
+        $items = Item::paginate(9);
 
         return response()->json([
             'success' => true,
